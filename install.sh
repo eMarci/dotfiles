ccf255dd68a8c01320c163850f18fcc5c85fbb16
Marton Endredi <endredimarci@gmail.com>
2026-04-19 13:54:40 +0200

    Add install script for entire repo

---
+++ install.sh
@@ -0,0 +1,5 @@
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin \
+    && export PATH="$HOME/.local/bin:$PATH" \
+    && chezmoi init https://github.com/eMarci/dotfiles.git \
+    && chezmoi apply \
+    && exec bash
