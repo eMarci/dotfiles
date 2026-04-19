21fed01604067c6982f2d4a9e411f00f36559c45
Marton Endredi <endredimarci@gmail.com>
2026-04-19 14:05:27 +0200

    Remove exec bash line from install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin \
     && export PATH="$HOME/.local/bin:$PATH" \
     && chezmoi init https://github.com/eMarci/dotfiles.git \
-    && chezmoi apply \
-    && exec bash
+    && chezmoi apply
